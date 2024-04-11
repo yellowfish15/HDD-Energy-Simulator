6a3173cf73d283b01d4bb1461f7708fb00766628
yellowfish15 <contactyulim@gmail.com>
2024-04-10 20:49:22 -0500

    finished algorithm simulator and added default and timeout algos

--- sim.py
+++ sim.py
@@ -1,4 +1,3 @@
-from abc import abstractmethod
 import pickle
 import HDD
 
@@ -17,8 +16,8 @@ class Algorithm:
         if self.backlog == 0 or interval == 0:
             return energy, wait, interval
         if self.wu_tr == 0 and self.sd_tr == 0:
-            if self.state == 2: # activate shutdown if in sleeping state
-                self.sd_tr = self.device.T_wu
+            if self.state == 2: # in shutdown state, so wake up
+                self.wu_tr = self.device.T_wu
             elif self.state == 1: # switch to active state
                 self.state = 0
         if self.wu_tr >= interval:
@@ -56,20 +55,23 @@ class Algorithm:
             wait += act_wait
         return energy, wait, interval
         
-
     def idle(self, interval):
-        energy = 0 # energy consumption in joules
-        wait = 0 # cumulative wait time across all requests
-        self.clear_backlog(interval) # clear any waiting requests
+        energy, wait, interval = self.clear_backlog(interval) # clear any waiting requests
+        if interval > 0: # backlog should be cleared
+            # turn to standby power
+            self.state = 1
+            idle_energy = self.run_algo(interval)
+            energy += idle_energy
+        return energy, wait
 
     def busy(self, interval):
+        if interval == 0:
+            return 0, 0
         energy = 0 # energy consumption in joules
         wait = 0 # cumulative wait time across all requests
-        if interval == 0:
-            return energy, wait
         if self.wu_tr == 0 and self.sd_tr == 0:
             if self.state == 2: # activate shutdown if in sleeping state
-                self.sd_tr = self.device.T_wu
+                self.wu_tr = self.device.T_wu
             elif self.state == 1: # switch to active state
                 self.state = 0
         if self.wu_tr >= interval:
@@ -97,7 +99,7 @@ class Algorithm:
             interval -= self.sd_tr
             self.sd_tr = 0 # reset the shut down timer
             self.state = 2 # sleeping state
-        elif self.state == 0:
+        elif self.state == 0: # active
             energy = interval*self.device.active_power
             wait = self.backlog
             interval = 0
@@ -107,6 +109,11 @@ class Algorithm:
             energy += act_energy
             wait += act_wait
         return energy, wait
+    
+    # abstract method to be implemented by the algorithms
+    def run_algo(self, interval):
+        # default behavior (no algorithm)
+        return interval*self.device.standby_power
 
 
 class Timeout(Algorithm):
@@ -115,8 +122,27 @@ class Timeout(Algorithm):
         Algorithm.__init__(self, device)
 
     # override
-    def idle(self, interval):
-        pass
+    def run_algo(self, interval):
+        if interval >= self.gamma:
+            energy = self.gamma*self.device.standby_power
+            interval -= self.gamma
+            # initiate shutdown
+            self.sd_tr = self.device.T_sd
+            if self.sd_tr >= interval: # T_sd covers entire interval
+                energy += interval*self.device.P_sd
+                self.sd_tr -= interval
+                interval = 0
+            else:
+                energy += self.sd_tr*self.device.P_sd
+                interval -= self.sd_tr
+                self.sd_tr = 0
+                self.state = 2
+                energy += interval*self.device.sleeping_power
+                interval = 0
+            return energy
+        else:
+            return interval*self.device.standby_power
+
 
 def run(A: Algorithm, W: list):
     total_consumption = 0
@@ -133,12 +159,18 @@ def run(A: Algorithm, W: list):
         total_consumption += energy_consumption
         total_wait_time += wait_time
     print("Statistics:")
-    print("Total Enery Consumption (Joules):", total_consumption)
-    print("Average wait time per request:", total_wait_time/request_count)
+    print("Total Enery Consumption (Joules):", f"{total_consumption:.4f}")
+    print("Average wait time per request (ms/request):", f"{total_wait_time/request_count:.4f}")
 
 
 # de-serialize workload file
 with open("wrkld", "rb") as f:
-    A = Timeout(HDD.A, 2500)
     W = pickle.load(f)
-    run(A, W)
+
+    print("--- Default Algorithm ---")
+    A_default = Algorithm(HDD.A)
+    run(A_default, W)
+
+    print("--- Timeout Algorithm ---")
+    A_timeout = Timeout(HDD.A, 1000)
+    run(A_timeout, W)
