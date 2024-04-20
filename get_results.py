143d3986158e4ffb0eb6fc43e5a88f8bfc83c4a9
yellowfish15 <contactyulim@gmail.com>
2024-04-20 16:35:16 -0500

    minor spelling correction on matplotlib axis unit

--- get_results.py
+++ get_results.py
@@ -21,7 +21,7 @@ with open("./results/results.pickle", "rb") as f:
                 values.append(stats["Energy"][i])
             ax.bar(index + i * bar_width, values, bar_width, label=ALGOS[i])
         ax.set_xlabel("Workload Distribution")
-        ax.set_ylabel("Energy Consumption (Watthours)")
+        ax.set_ylabel("Energy Consumption (Watt-hours)")
         ax.set_title("Total Energy Consumption " + drive.name)
         ax.set_xticks(index + bar_width * (num_algos - 1) / 2)
         ax.set_xticklabels(WORKLOADS)
